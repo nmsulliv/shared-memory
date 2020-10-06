dc166c428bd23a5521f0ebcd9f398de375044973
Nicole Sullivan <nicolesullivanmarie@gmail.com>
2020-10-06 23:59:06 +0000

    Implement dear old dad and poor student rules

--- bank.c
+++ bank.c
@@ -3,17 +3,20 @@
 #include  <sys/types.h>
 #include  <sys/ipc.h>
 #include  <sys/shm.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include  <unistd.h>
+#include  <sys/wait.h>
+#include  <time.h> 
+
+void  deposit(int* bankAccount);
+void  request(int* bankAccount);
 
 int  main(int  argc, char *argv[]) {
 		int ShmID;
 		int *ShmPTR;
 		pid_t  pid;
-// 		int    status;
 		int* bankAccount;
 		int* turn;
-
+	
 		ShmID = shmget(IPC_PRIVATE, 2*sizeof(int), IPC_CREAT | 0666);
 		if (ShmID < 0) {
 			printf("*** shmget error ***\n");
@@ -33,39 +36,67 @@ int  main(int  argc, char *argv[]) {
 		
 		bankAccount = &ShmPTR[0];
 		turn = &ShmPTR[1];
-	
+
+		srand(time(0));
+
 		pid = fork();
 		
 		if (pid > 0) { // parent process
 			int loops = 0;
 
 			while (loops < 25) {
-				while (*turn == 0);
-				printf("i'm dear old dad bank acc = $%d\n", *bankAccount);
-				*bankAccount += 10;
-				*turn = 0;
-				loops++;
-				printf("just added 10 to bank acc = $%d\n", *bankAccount);
+				int randSleep = (rand() % 1);
+				sleep(randSleep);
+
+				while (*turn == 0); 
+				if (*bankAccount <= 100) { // critical section
+					deposit(bankAccount);
+				} else {
+					printf("Dear old Dad: Thinks Student has enough Cash ($%d)\n", *bankAccount);
+				}
+				*turn = 0; // change turn
+				loops++; // non-critical section
 			}
+				wait(&pid);
+				printf("Process has detected the completion of its child...\n");
+				shmdt((void *) ShmPTR);
+				printf("Process has detached its shared memory...\n");
+				shmctl(ShmID, IPC_RMID, NULL);
+				printf("Process has removed its shared memory...\n");
+				printf("Process exits...\n");
+				exit(0);
 		} else {
 			int loops = 0;
 
 			while (loops < 25) {
+				int randSleep = (rand() % 1);
+				sleep(randSleep);
 				while (*turn == 1);
-				printf("i'm poor student :( bank acc = $%d\n", ShmPTR[0]);
-				*turn = 1;
-				loops++;
-				*bankAccount-=1;
-				printf("just subtracted 1 bank acc = $%d\n", ShmPTR[0]);
+				request(bankAccount); // critical section
+				*turn = 1; // change turn
+				loops++; // non-critical section
 			}
+			exit(0);
 		}
-
-	 printf("Process has detected the completion of its child...\n");
-	 shmdt((void *) ShmPTR);
-	 printf("Process has detached its shared memory...\n");
-	 shmctl(ShmID, IPC_RMID, NULL);
-	 printf("Process has removed its shared memory...\n");
-	 printf("Process exits...\n");
-	 exit(0);
 }
 
+void deposit(int* bankAccount) {
+	int randMoney = (rand() % 100);
+	if ((randMoney % 2) == 0) {
+		*bankAccount += randMoney;
+		printf("Dear old Dad: Deposits $%d / Balance = $%d\n", randMoney, *bankAccount);
+	} else {
+		printf("Dear old Dad: Doesn't have any money to give\n");
+	}
+}
+
+void request(int* bankAccount) {
+	int requested = (rand() % 50);
+	printf("Poor Student needs $%d\n", requested);
+	if (requested <= *bankAccount) {
+		*bankAccount -= requested;
+		printf("Poor Student: Withdraws $%d / Balance = $%d\n", requested, *bankAccount);
+	} else {
+		printf("Poor Student: Not Enough Cash ($%d)\n", *bankAccount);
+	}
+}
